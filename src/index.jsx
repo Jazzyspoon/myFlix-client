f0fe0fbf743d3353d20fc8725d4c9dc574fa9065
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-27 19:25:37 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+import "./index.scss";
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
@@ -16,7 +16,7 @@ class MyFlixApplication extends React.Component {
 }
 
 // Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
