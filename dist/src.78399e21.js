9c7b3e5d61dde439d45d8c05fa22e96031b3d1f8
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-30 18:26:33 -0600

    can't get image to load

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29764,28 +29764,28 @@ var MainView = /*#__PURE__*/function (_React$Component) {
         Description: "A Bladerunner must track down and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
         Genre: "sci-fi",
         Director: "Ridley Scott",
-        ImagePath: "/src/img/bladerunner.jpg"
+        ImagePath: "/public/bladerunner.jpg"
       }, {
         _id: "604f9e3505951614efe12f3a",
         Title: "Alien",
         Description: "After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun.",
         Genre: "sci-fi",
         Director: "Ridley Scott",
-        ImagePath: "/src/img/alien.jpg"
+        ImagePath: "/public/alien.jpg"
       }, {
         _id: "604f9e5805951614efe12f3b",
         Title: "The Chronicles Of Riddick",
         Description: "The wanted criminal Richard Bruno Riddick (Vin Diesel) arrives on a planet called Helion Prime and finds himself up against an invading empire called the Necromongers, an army that plans to convert or kill all humans in the universe.",
         Genre: "sci-fi",
         Director: "David Twohy",
-        ImagePath: "/src/img/riddick.jpg"
+        ImagePath: "/public/riddick.jpg"
       }, {
         _id: "604f9e6505951614efe12f3c",
         Title: "The Avengers",
         Description: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
         Genre: "action",
         Director: "Joss Whedon",
-        ImagePath: "/src/img/avengers.jpg"
+        ImagePath: "/public/avengers.jpg"
       }],
       selectedMovie: null
     };
--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -14,7 +14,7 @@ export default class MainView extends React.Component {
             "A Bladerunner must track down and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
           Genre: "sci-fi",
           Director: "Ridley Scott",
-          ImagePath: "/src/img/bladerunner.jpg",
+          ImagePath: "/public/bladerunner.jpg",
         },
         {
           _id: "604f9e3505951614efe12f3a",
@@ -23,7 +23,7 @@ export default class MainView extends React.Component {
             "After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun.",
           Genre: "sci-fi",
           Director: "Ridley Scott",
-          ImagePath: "/src/img/alien.jpg",
+          ImagePath: "/public/alien.jpg",
         },
         {
           _id: "604f9e5805951614efe12f3b",
@@ -32,7 +32,7 @@ export default class MainView extends React.Component {
             "The wanted criminal Richard Bruno Riddick (Vin Diesel) arrives on a planet called Helion Prime and finds himself up against an invading empire called the Necromongers, an army that plans to convert or kill all humans in the universe.",
           Genre: "sci-fi",
           Director: "David Twohy",
-          ImagePath: "/src/img/riddick.jpg",
+          ImagePath: "/public/riddick.jpg",
         },
         {
           _id: "604f9e6505951614efe12f3c",
@@ -41,7 +41,7 @@ export default class MainView extends React.Component {
             "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
           Genre: "action",
           Director: "Joss Whedon",
-          ImagePath: "/src/img/avengers.jpg",
+          ImagePath: "/public/avengers.jpg",
         },
       ],
       selectedMovie: null,
