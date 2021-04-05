89b608c54d23166ea015710aa7b9bbd37bd795cf
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-05 09:40:51 -0600

    updating for 2.4

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,50 +7,21 @@ export default class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: "604f9e2305951614efe12f39",
-          Title: "Bladerunner",
-          Description:
-            "A Bladerunner must track down and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
-          Genre: "sci-fi",
-          Director: "Ridley Scott",
-          ImagePath:
-            "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        },
-        {
-          _id: "604f9e3505951614efe12f3a",
-          Title: "Alien",
-          Description:
-            "After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun.",
-          Genre: "sci-fi",
-          Director: "Ridley Scott",
-          ImagePath:
-            "https://m.media-amazon.com/images/M/MV5BMmQ2MmU3NzktZjAxOC00ZDZhLTk4YzEtMDMyMzcxY2IwMDAyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        },
-        {
-          _id: "604f9e5805951614efe12f3b",
-          Title: "The Chronicles Of Riddick",
-          Description:
-            "The wanted criminal Richard Bruno Riddick (Vin Diesel) arrives on a planet called Helion Prime and finds himself up against an invading empire called the Necromongers, an army that plans to convert or kill all humans in the universe.",
-          Genre: "sci-fi",
-          Director: "David Twohy",
-          ImagePath:
-            "https://m.media-amazon.com/images/M/MV5BNzBjNmJkYjUtMTFjMC00ZWI5LWEyM2YtNzczOTczMmM1ODY5XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        },
-        {
-          _id: "604f9e6505951614efe12f3c",
-          Title: "The Avengers",
-          Description:
-            "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-          Genre: "action",
-          Director: "Joss Whedon",
-          ImagePath:
-            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
+  }
+  componentDidMount() {
+    axios
+      .get("https://movieflixappjp.herokuapp.com/movies")
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -81,9 +53,9 @@ export default class MainView extends React.Component {
         {movies.map((movie) => (
           <MovieCard
             key={movie._id}
-            movieData={movie}
-            onMovieClick={(movie) => {
-              this.setSelectedMovie(movie);
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              this.setState({ selectedMovie: newSelectedMovie });
             }}
           />
         ))}
