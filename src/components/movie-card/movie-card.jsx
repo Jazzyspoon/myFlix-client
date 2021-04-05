48550898efcc2c54a36e3600330a4c379d567832
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-05 14:12:42 -0600

    setting up requirements for task 3.4

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
+    const { movie, onClick } = this.props;
+
     return (
-      <div
-        className="movie-card"
-        onClick={() => {
-          onMovieClick(movieData);
-        }}
-      >
+      <div onClick={() => onClick(movie)} className="movie-card">
         {movieData.Title}
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({ Name: PropTypes.string.isRequired }).isRequired,
+    Genre: PropTypes.shape({ Name: PropTypes.string.isRequired }).isRequired,
+    Actors: PropTypes.string.isRequired,
+  }).isRequired,
+
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,43 +1,46 @@
 import React from "react";
-
-export class MovieView extends React.Component {
-  componentDidMount() {
-    document.addEventListener("keypress", (event) => {
-      console.log(event.key);
-    });
-  }
+import "./movie-view.scss";
+export class MovieCard extends React.Component {
   render() {
-    const { movieData, onBackClick } = this.props;
+    const { movie, onClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movieData.ImagePath} alt={movieData.Title} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movieData.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movieData.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movieData.Genre.Name}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movieData.Director.Name}</span>
-        </div>
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </button>
+      <div onClick={() => onClick(movie)} className="movie-card">
+        {movieData.Title}
       </div>
     );
   }
 }
+
+//     return (
+//       <div className="movie-view">
+//         <div className="movie-poster">
+//           <img src={movieData.ImagePath} alt={movieData.Title} />
+//         </div>
+//         <div className="movie-title">
+//           <span className="label">Title: </span>
+//           <span className="value">{movieData.Title}</span>
+//         </div>
+//         <div className="movie-description">
+//           <span className="label">Description: </span>
+//           <span className="value">{movieData.Description}</span>
+//         </div>
+//         <div className="movie-genre">
+//           <span className="label">Genre: </span>
+//           <span className="value">{movieData.Genre.Name}</span>
+//         </div>
+//         <div className="movie-director">
+//           <span className="label">Director: </span>
+//           <span className="value">{movieData.Director.Name}</span>
+//         </div>
+//         <button
+//           onClick={() => {
+//             onBackClick(null);
+//           }}
+//         >
+//           Back
+//         </button>
+//       </div>
+//     );
+//   }
+// }
