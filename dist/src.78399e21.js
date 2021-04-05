d16e9ba52e3e0089b7681b375961da994586e4c1
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-05 15:57:53 -0600

    dist updates

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33061,11 +33061,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< Updated upstream
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64497" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64486" + '/');
->>>>>>> Stashed changes
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64509" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
