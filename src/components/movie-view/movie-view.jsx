45d6e6d660aa11466254befa6dbc688e5977c1d3
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-08 10:18:17 -0600

    last minute changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -15,10 +15,6 @@ export class MovieView extends React.Component {
   goBack() {
     window.open("/", "_self");
   }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   render() {
     const { movieData: movie, onClick } = this.props;
     if (!movie) return null;
