747c229f456d7d797a0bf074f341c303330eae80
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-22 14:16:49 -0600

    fixing the register view issue on hosted site

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -84,5 +84,5 @@ LoginView.propTypes = {
     Username: PropTypes.string.isRequired,
     Password: PropTypes.string.isRequired,
   }),
-  onLoggedIn: PropTypes.func.isRequired,
+  onLoggedIn: PropTypes.func,
 };
