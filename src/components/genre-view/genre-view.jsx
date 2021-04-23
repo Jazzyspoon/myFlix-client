571954f427ddc4386c8215072dcb6d0818283638
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-23 10:26:22 -0600

    fixed genre view images

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -20,10 +20,10 @@ export class GenreView extends React.Component {
         <div>
           <div className="card mb-3 cardbody">
             <div className="row no-gutters">
-              <div className="col-md-3">
+              <div className="col-md-5">
                 <Image src={genre.Genre.ImagePath} className="image" fluid />
               </div>
-              <div className="col-md-8">
+              <div className="col-md-7">
                 <Card.Body>
                   <Card.Text as="h1">{genre.Genre.Name}</Card.Text>
                   <Card.Text>Description: {genre.Genre.Description}</Card.Text>
